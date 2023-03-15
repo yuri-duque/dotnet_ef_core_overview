aad5ffeac227924f8d6a763bba3e45d33d21ec9b
yuri-duque <yurithielmann83@gmail.com>
2023-03-15 19:01:00 -0300

    Creating UserUpdateDTO

---
+++ Controller/Dtos/User/UserUpdateDTO.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Interfaces;
+
+namespace Controller.Dtos.User
+{
+    public record UserUpdateDTO : IUser
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+    }
+}
