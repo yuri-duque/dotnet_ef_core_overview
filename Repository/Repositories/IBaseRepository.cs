7d05aaae1b8f7dbfc418dee2cfbaf0740200ff11
yuri-duque <yurithielmann83@gmail.com>
2023-03-15 00:41:40 -0300

    creating a base repository

---
+++ Repository/Repositories/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Repository.Repositories
+{
+    public interface IBaseRepository<TEntity>
+    {
+        IQueryable<TEntity> GetAll();
+        IQueryable<TEntity> Get(Func<TEntity, bool> predicate);
+        TEntity Find(params object[] key);
+        void Update(TEntity obj);
+        void SaveAll();
+        void Add(TEntity obj);
+        void Delete(Func<TEntity, bool> predicate);
+    }
+}
