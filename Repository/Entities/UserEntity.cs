14777cc51fdad4b62e980d7648cbdccf957cc8b3
yuri-duque <yurithielmann83@gmail.com>
2023-03-15 00:43:27 -0300

    creating UserEntity

---
+++ Repository/Entities/UserEntity.cs
@@ -0,0 +1,52 @@
+﻿using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Repository.Entities
+{
+    public class UserEntity : IUser
+    {
+        [Key]
+        public string Id { get; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        #region Relations
+
+        #endregion
+
+        #region Constructors
+
+        public UserEntity(string email, string name)
+        {
+            Id = Guid.NewGuid().ToString();
+            Email = email;
+            Name = name;
+        }
+
+        public UserEntity(string id, string email, string name)
+        {
+            Id = id;
+            Email = email;
+            Name = name;
+        }
+
+        #endregion
+
+        #region Mapping
+
+        public static void Map(ModelBuilder modelBuilder)
+        {
+            var map = modelBuilder.Entity<UserEntity>();
+            map.HasIndex(x => x.Id).IsUnique();
+
+            map.HasIndex(x => x.Email).IsUnique();
+        }
+
+        #endregion
+    }
+}
