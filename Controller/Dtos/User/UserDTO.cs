d3e164018514350f5a1b265fdac97b467e3e2713
yuri-duque <yurithielmann83@gmail.com>
2023-03-15 19:00:37 -0300

    Creating userDTO

---
+++ Controller/Dtos/User/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Interfaces;
+
+namespace Controller.Dtos.User
+{
+    public record UserDTO : IBaseEntity, IUser
+    {
+        public string Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+
+        public string Email { get; set; }
+        public string Name { get; set; }
+    }
+}
