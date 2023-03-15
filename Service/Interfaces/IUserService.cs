c0623f80124e13616a00a90bd94afe04e51b8870
yuri-duque <yurithielmann83@gmail.com>
2023-03-15 18:59:32 -0300

    creatin UserService interface

---
+++ Service/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Service.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IList<User>> GetAll();
+        Task<User> FindById(string id);
+        Task Save(User user);
+        Task Update(User user);
+        Task Delete(string id);
+    }
+}
