98079714231a4c0426e1c72c902326d30b676122
yuri-duque <yurithielmann83@gmail.com>
2023-03-15 00:42:42 -0300

    changing context to BaseContext

---
+++ Repository/Context/BaseContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Repository.Entities;
+
+namespace Repository.Context
+{
+    public class BaseContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public BaseContext(DbContextOptions<BaseContext> options, IConfiguration configuration) : base(options)
+        {
+            this._connectionString = configuration.GetConnectionString("Default") ?? "";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            UserEntity.Map(modelBuilder);
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+    }
+}
