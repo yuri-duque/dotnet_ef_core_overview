67cef4a88995551e2012bb91224f259387a558c4
yuri-duque <yurithielmann83@gmail.com>
2023-03-15 19:01:59 -0300

    using IBaseUser on User class

--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -2,9 +2,11 @@
 
 namespace Domain.Entities
 {
-    public class User : IUser
+    public class User : IBaseEntity, IUser
     {
         public string Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
         public string Email { get; set; }
         public string Name { get; set; }
     }
