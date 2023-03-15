8df7c38d74199ecd3be8750d292ca8824b0261d7
yuri-duque <yurithielmann83@gmail.com>
2023-03-15 19:03:52 -0300

    Creating a CRUD methods on UserRepository

--- Repository/Repositories/UserRepository.cs
+++ Repository/Repositories/UserRepository.cs
@@ -26,5 +26,33 @@ namespace Repository.Repositories
 
             return users;
         }
+
+        public async Task<User> FindById(string id)
+        {
+            var baseUser = await _repository.Find(id);
+
+            var user = _mapper.Map<User>(baseUser);
+
+            return user;
+        }
+
+        public async Task Save(User user)
+        {
+            var newUser = _mapper.Map<UserEntity>(user);
+
+            await _repository.Save(newUser);
+        }
+
+        public async Task Update(User user)
+        {
+            var updateUser = _mapper.Map<UserEntity>(user);
+
+            await _repository.Update(updateUser);
+        }
+
+        public async Task Delete(string id)
+        {
+            await _repository.Delete(x => x.Id == id);
+        }
     }
 }
