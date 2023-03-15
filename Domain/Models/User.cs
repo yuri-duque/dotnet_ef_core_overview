6adc53501805a6ccd21e96a434de27bd096fb98d
yuri-duque <yurithielmann83@gmail.com>
2023-03-15 00:43:41 -0300

    adding email on user model

---
+++ Domain/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Interfaces;
+
+namespace Domain.Entities
+{
+    public class User : IUser
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+    }
+}
