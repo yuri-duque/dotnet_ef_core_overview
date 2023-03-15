f1d07a6bc81398694bcfab9e9784498ecfcc4473
yuri-duque <yurithielmann83@gmail.com>
2023-03-15 19:03:12 -0300

    Creating methods GetAll and FindById on controller

--- Controller/Controllers/UserController.cs
+++ Controller/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-using Domain.Entities;
+using AutoMapper;
+using Controller.Dtos.User;
 using Microsoft.AspNetCore.Mvc;
 using Repository.Interfaces;
 
@@ -8,21 +9,35 @@ namespace Controller.Controllers
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        private IUserRepository _userRepository;
         private readonly ILogger<UserController> _logger;
+        private IMapper _mapper;
+        private IUserRepository _userRepository;
 
-        public UserController(IUserRepository userRepository, ILogger<UserController> logger)
+        public UserController(ILogger<UserController> logger, IMapper mapper, IUserRepository userRepository)
         {
             _logger = logger;
+            _mapper = mapper;
             _userRepository = userRepository;
         }
 
         [HttpGet]
-        public async Task<IEnumerable<User>> Get()
+        public async Task<IEnumerable<UserDTO>> GetAll()
         {
             var users = await _userRepository.GetAll();
 
-            return users;
+            var usersDTO = _mapper.Map<IEnumerable<UserDTO>>(users);
+
+            return usersDTO;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<UserDTO> findById([FromQuery] string id)
+        {
+            var user = await _userRepository.FindById(id);
+
+            var userDTO = _mapper.Map<UserDTO>(user);
+
+            return userDTO;
         }
     }
 }
