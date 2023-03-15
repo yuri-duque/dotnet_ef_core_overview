580a1d9e123fdecee85c0e56e7bb0a1a60a0e317
yuri-duque <yurithielmann83@gmail.com>
2023-03-15 01:03:01 -0300

    creating service project

---
+++ Service/Services/UserService.cs
@@ -0,0 +1,43 @@
+﻿using Repository.Repositories;
+
+namespace Service.Services
+{
+    public class UserService
+    {
+        private readonly UserRepository _productRepository;
+
+        public UserService(UserRepository produtoRepository)
+        {
+            _productRepository = produtoRepository;
+        }
+
+        public IList<Product> GetAll()
+        {
+            var products = _productRepository.GetAll();
+
+            return products.ToList();
+        }
+
+        public object GetById(long id)
+        {
+            var product = _productRepository.Find(id);
+
+            return product;
+        }
+
+        public void Save(Product product)
+        {
+            _productRepository.Save(product);
+        }
+
+        public void Update(Product product)
+        {
+            _productRepository.Update(product);
+        }
+
+        public void Delete(long id)
+        {
+            _productRepository.Delete(x => x.Id == id);
+        }
+    }
+}
