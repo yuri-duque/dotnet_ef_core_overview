1a51a8aa5d866ad83cf108d2f662c40f34397def
yuri-duque <yurithielmann83@gmail.com>
2023-03-15 19:02:52 -0300

    Updating IUserRepository

--- Repository/Interfaces/IUserRepository.cs
+++ Repository/Interfaces/IUserRepository.cs
@@ -5,5 +5,9 @@ namespace Repository.Interfaces
     public interface IUserRepository
     {
         Task<IEnumerable<User>> GetAll();
+        Task<User> FindById(string id);
+        Task Save(User user);
+        Task Update(User user);
+        Task Delete(string id);
     }
 }
