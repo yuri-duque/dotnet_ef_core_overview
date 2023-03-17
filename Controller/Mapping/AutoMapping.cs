cf3af1f30b7fbe9fef3b8ada00346436447fdcbf
yuri-duque <yurithielmann83@gmail.com>
2023-03-17 20:06:15 -0300

    Creating automapper files

---
+++ Controller/Mapping/AutoMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Repository.Mapping;
+
+namespace Controller.Mapping
+{
+    public class AutoMapping : Profile
+    {
+        public AutoMapping()
+        {
+            new RepositoryMapping();
+            new DtosMapping();
+        }
+    }
+}
---
+++ Controller/Mapping/DtosMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Controller.Dtos.User;
+using Domain.Entities;
+
+namespace Controller.Mapping
+{
+    public class DtosMapping : Profile
+    {
+        public DtosMapping()
+        {
+            CreateMap<User, UserDTO>().ReverseMap();
+            CreateMap<User, UserSaveDTO>().ReverseMap();
+            CreateMap<User, UserUpdateDTO>().ReverseMap();
+        }
+    }
+}
