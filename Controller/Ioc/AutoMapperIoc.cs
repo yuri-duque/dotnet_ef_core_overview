9af1d1f029f74c36669c4654156ced5acfceefe5
yuri-duque <yurithielmann83@gmail.com>
2023-03-17 20:04:06 -0300

    creating automapper IOC

---
+++ Controller/Ioc/AutoMapperIoc.cs
@@ -0,0 +1,14 @@
+﻿using Controller.Mapping;
+
+namespace Repository.Ioc
+{
+    public static class AutoMapperIoc
+    {
+        public static IServiceCollection ConfigureAutoMapper(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(AutoMapping));
+
+            return services;
+        }
+    }
+}
