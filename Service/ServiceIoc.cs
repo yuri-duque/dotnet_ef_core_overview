8bcebc0f3e03e42726e6f279cfd2d42e0dd04042
yuri-duque <yurithielmann83@gmail.com>
2023-03-17 20:04:29 -0300

    Creating a service IOC

---
+++ Service/ServiceIoc.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Service.Services;
+using Service.Services.Interfaces;
+
+namespace Service
+{
+    public static class ServiceIoc
+    {
+        public static IServiceCollection ConfigureService(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+
+            return services;
+        }
+    }
+}
