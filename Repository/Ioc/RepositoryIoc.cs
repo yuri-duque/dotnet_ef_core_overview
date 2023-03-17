cb4d6b358c3b7f093dc314bca315c9450b5e00b5
yuri-duque <yurithielmann83@gmail.com>
2023-03-17 20:04:41 -0300

    Creating a repository IOC

---
+++ Repository/Ioc/RepositoryIoc.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Repository.Context;
+using Repository.Repositories;
+using Repository.Repositories.Interfaces;
+
+namespace Repository.Ioc
+{
+    public static class EFIoc
+    {
+        public static IServiceCollection ConfigureRepository(this IServiceCollection services)
+        {
+            services.AddDbContext<BaseContext>();
+
+            #region DependenciInjection
+
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            #endregion
+
+            return services;
+        }
+    }
+}
