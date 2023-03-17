37c2e7e08afc822f2ca8153a3ee90161b02be013
yuri-duque <yurithielmann83@gmail.com>
2023-03-17 20:04:16 -0300

    Creating Controller IOC

---
+++ Controller/Ioc/ControllerIoc.cs
@@ -0,0 +1,13 @@
+﻿namespace Repository.Ioc
+{
+    public static class ControllerIoc
+    {
+        public static IServiceCollection ConfigureController(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            return services;
+        }
+    }
+}
